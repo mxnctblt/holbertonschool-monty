65297b422ef952be957655cc9716b8e583d40c4c
AntoineJacob <5646@holbertonstudents.com>
2022-12-12 05:14:41 -0800

    Adding the v1 of pall and push

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pall - function that print all elements in stack
+ * @stack: a pointer on the head of stack
+ * @line_number: the number of the line
+ * Return: return
+ */
+
+void pall (stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+	(void) line_number;
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->n;
+	}
+}
---
+++ push.c
@@ -0,0 +1,34 @@
+/**
+ * push - function that push an element on the stack
+ * @stack: pointer to the head stack.
+ * @line_number: line number in file.
+ * Return: nothing.
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = NULL;
+	stack_t *copy = *stack;
+	(void)line_number;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		var_glob[1] = 1;
+		return;
+	}
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(stack);
+		var_glob[1] = 1;
+		return;
+	}
+	node->prev = NULL;
+	node->n = var_glob[0];
+	node->next = *stack;
+	if (*stack)
+		copy->prev = node;
+	*stack = node;
+}
